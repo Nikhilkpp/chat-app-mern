f616ad21678ddf320a9c58297175ca2295746a73
Nikhilkpp <nikhilpathak781@gmail.com>
2025-06-07 16:00:41 +0530

    sm

--- f/src/contexts/SocketContext.jsx
+++ f/src/contexts/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider= ({children})=> {
     const {authUser}=useAuthContext();
     useEffect(() => {
         if(authUser){
-            const socket=io("http://localhost:8000", {
+            const socket=io("https://chat-app-mern-kx4c.onrender.com", {
                 query:{
                     userId: authUser._id,
                 }
