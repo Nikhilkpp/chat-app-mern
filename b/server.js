931989f59615b7fa807475fba0aebd94c5e8987e
Nikhilkpp <nikhilpathak781@gmail.com>
2025-06-07 15:49:24 +0530

    sm

--- b/server.js
+++ b/server.js
@@ -1,3 +1,60 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import authRouter from "./routes/auth.routes.js";
+// import connectToMongo from "./db/connectToMongo.js";
+// import messageRouter from "./routes/message.routes.js";
+// import userRouter from "./routes/user.routes.js";
+// import cookieParser from "cookie-parser";
+// import path from 'path';
+// import cors from 'cors'
+// import { app, server } from "./socket/socket.js";
+
+
+// dotenv.config()
+
+// const PORT=process.env.PORT || 5000;
+
+// const __dirname = path.resolve();
+
+// // const app=express();    socket se aarha
+
+// // app.get('/', (req,res)=>{
+// //     res.send("Hello world again!")
+// // })
+
+// app.use(express.json())
+// app.use(cookieParser())
+
+// app.use(cors({
+//     origin:process.env.CORS_ORIGIN,
+
+//     credentials:true
+// }))
+
+// server.listen(PORT,()=> {
+//     connectToMongo()
+//     console.log("Server is listing",PORT
+        
+//     )});
+// app.use("/api/auth",authRouter)
+// app.use("/api/messages", messageRouter)
+// app.use("/api/users", userRouter)
+// app.use(express.static(path.join(__dirname, "/f/dist")));
+
+// app.get("", (req,res)=>{
+//     res.sendFile(path.join(__dirname, "f", "diat", "index.html"))
+// })
+
+
+
+
+
+
+
+
+
+
+
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes.js";
@@ -7,7 +64,8 @@ import userRouter from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import path from 'path';
 import cors from 'cors'
-import { app, server } from "./socket/socket.js";
+import { initSocket } from "./socket/socket.js";
+import http from 'http';
 
 
 dotenv.config()
@@ -16,8 +74,9 @@ const PORT=process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-// const app=express();    socket se aarha
-
+const app=express();   // socket se aarha
+const server = http.createServer(app);
+initSocket(server)
 // app.get('/', (req,res)=>{
 //     res.send("Hello world again!")
 // })
@@ -31,16 +90,17 @@ app.use(cors({
     credentials:true
 }))
 
-server.listen(PORT,()=> {
-    connectToMongo()
-    console.log("Server is listing",PORT
-        
-    )});
+
 app.use("/api/auth",authRouter)
 app.use("/api/messages", messageRouter)
 app.use("/api/users", userRouter)
 app.use(express.static(path.join(__dirname, "/f/dist")));
 
-app.get("", (req,res)=>{
-    res.sendFile(path.join(__dirname, "f", "diat", "index.html"))
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "f", "dist", "index.html"))
 })
+
+server.listen(PORT,()=> {
+    connectToMongo();
+    console.log("Server is listening",PORT);
+});
